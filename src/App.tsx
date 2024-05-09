82099395b2a7ea9cf66fdc16df8153fa68393544
lemolatoon <63438515+lemolatoon@users.noreply.github.com>
2024-05-09 14:30:48 +0900

    Add sounds buttons

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,25 @@
-import { VStack } from "@chakra-ui/react";
+import { Box, Button, HStack, VStack } from "@chakra-ui/react";
 import { Header } from "./components/header";
+import chips from "/chips.mp3";
+import useSound from "use-sound";
 
 function App() {
+  const [play, { stop }] = useSound(chips, { loop: true });
   return (
     <>
       <VStack>
         <Header />
+
+        <Box mt="8">
+          <HStack gap="32">
+            <Button colorScheme="yellow" size="lg" onClick={() => play()}>
+              Play
+            </Button>
+            <Button colorScheme="yellow" size="lg" onClick={() => stop()}>
+              Stop
+            </Button>
+          </HStack>
+        </Box>
       </VStack>
     </>
   );
